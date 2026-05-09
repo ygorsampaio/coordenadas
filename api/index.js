83ef0c87e49428eae44de55415ff0ded84603dc2
ygorsampaio <ygorsampaio.edu@gmail.com>
2026-05-08 21:42:26 -0300

    Fix Vercel routing and restore API handler for /api/locations

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+﻿const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const locationsRouter = require('../backend/routes/locations');
